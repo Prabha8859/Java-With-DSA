6da65ebc5fcaab1f6a9cb2f96cc8fe5f77c796f3
@PrabhaSingh <123654346+Prabha8859@users.noreply.github.com>
2023-05-03 08:35:33 +0530

    Add files via upload

---
+++ First-Assignment/AssenSecond.java
@@ -0,0 +1,10 @@
+public class AssenSecond {
+    public static void main(String[] args) {
+        int [] arr= {34,21,54,65,43};
+        for(int i:arr){
+            if(i%2 ==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
