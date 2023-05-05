a9690b0a5f846d7c911b258125d6ab06b1e073be
@PrabhaSingh <123654346+Prabha8859@users.noreply.github.com>
2023-05-05 16:27:03 +0530

    Update InsertionSort.java

--- Second-Assingment/Sorting-Array/InsertionSort.java
+++ Second-Assingment/Sorting-Array/InsertionSort.java
@@ -4,15 +4,18 @@
 import java.util.*;
 public class InsertionSort {
     public static void insertionSort(int arr[]){
+        int count = 0;
         for(int i=1; i<arr.length; i++){
             int j=i;
             while(j>0 && arr[j] > arr[j-1]){
+                count++;
                 int temp = arr[j];
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
                 j--;
             }
         }
+        System.out.print("This Counting Loop :" +count);
     }
     public static void main(String[] args) {
         int arr [] = {3,5,1,6,0};
